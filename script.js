40762c7ee5b749b3b1d28ba995547ece81a2aa41
Skibidiyous13 <frissezyt@gmail.com>
2025-05-15 19:21:39 +0200

    Premier commit de MoonZone

---
+++ script.js
@@ -0,0 +1,6 @@
+// Exemple de fonction pour un bouton de jeu
+document.querySelectorAll("button").forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("Jeu lancé depuis MoonZone!");
+    });
+});
